2a66e918fcf9683438759835131d256a01bfc70e
Shertone-V1 <mikulajakubko@gmail.com>
2025-03-10 22:52:12 +0100

    update

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -5,7 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-
+builder.WebHost.UseWebRoot("wwwroot");
+builder.WebHost.UseStaticWebAssets();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
--- Snake/obj/Debug/net9.0/Snake.AssemblyInfo.cs
+++ Snake/obj/Debug/net9.0/Snake.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Snake")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+579870902ec8ab5cb32aa2c15f04a216c08bbe09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8307a49a0c97a49fd2ce3afa7ce6b42581109b0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Snake")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Snake")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
